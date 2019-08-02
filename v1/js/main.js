1a519899c5b4bc314930c47ece52a2c95429c604
zaidun <zaidun10@yahoo.com>
2019-08-02 15:27:35 +0200

    g

--- v1/js/main.js
+++ v1/js/main.js
@@ -8,6 +8,7 @@ $(function(){
             $('#childLastname').text(data[0].firsname);
             $('#childAge').text(data[0].picture);
             $('#description').text(data[0].description);
+            $('#sex').text(data[0].sex);
             
          /**
           * @end
