04d1f878c6faa75225cc10a2b33ad684ee13da5c
bastienhousiaux <bastienhousiaux@yahoo.fr>
2019-08-03 01:19:02 +0200

    ajout d'une fonction pour créer un cercle

--- v1/js/mapVolunteer.js
+++ v1/js/mapVolunteer.js
@@ -1,3 +1,42 @@
+var createGeoJSONCircle = function(center, radiusInKm, points) {
+    if(!points) points = 64;
+
+    var coords = {
+        latitude: center[1],
+        longitude: center[0]
+    };
+
+    var km = radiusInKm;
+
+    var ret = [];
+    var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
+    var distanceY = km/110.574;
+
+    var theta, x, y;
+    for(var i=0; i<points; i++) {
+        theta = (i/points)*(2*Math.PI);
+        x = distanceX*Math.cos(theta);
+        y = distanceY*Math.sin(theta);
+
+        ret.push([coords.longitude+x, coords.latitude+y]);
+    }
+    ret.push(ret[0]);
+
+    return {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [ret]
+                }
+            }]
+        }
+    };
+};
+
 $(function(){
     var map;
     var postersData;
@@ -33,7 +72,10 @@ $(function(){
                     json:postersData
                 });
             })
+            
         });
+
+
         } else {
             alert("Le service de géolocalisation n'est pas disponible sur votre ordinateur.");
         }
@@ -50,6 +92,20 @@ $(function(){
                         new mapboxgl.Marker().setLngLat([posters[j].lng, posters[j].lat]).addTo(map);
                     }
                 }
+
+                //création d'un cercle test
+                map.addSource("polygon", createGeoJSONCircle([3.5119444444444445,50.6079444], 0.5));
+
+                map.addLayer({
+                    "id": "polygon",
+                    "type": "fill",
+                    "source": "polygon",
+                    "layout": {},
+                    "paint": {
+                        "fill-color": "blue",
+                        "fill-opacity": 0.6
+                    }
+                });
             });
         },1000);
 
