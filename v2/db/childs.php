b2ce32ed4c4ce4b0482ee2ec6b563ef0f20f06f3
Bidou6 <julien.catoul@hotmail.com>
2019-08-03 15:39:57 +0200

    Update childs.php

---
+++ v2/db/childs.php
@@ -0,0 +1,16 @@
+<?php
+require("connectdblib.php");
+require("cors.php");
+header('Content-Type: application/json');
+cors();
+
+
+
+$bdd=connectdb('hitw',true);
+
+$sql="SELECT * FROM childs";
+$request= $bdd -> query($sql);
+
+$row=$request->fetch(PDO::FETCH_ASSOC);
+
+echo json_encode($row);
