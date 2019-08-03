9db7de1117366c02404125aa88994abd628bc3d0
zaidun <zaidun10@yahoo.com>
2019-08-03 16:46:31 +0200

    update poster

--- v2/db/create_poster.php
+++ v2/db/create_poster.php
@@ -1,89 +1,39 @@
 <?php
-
-/* On ajoute la finction cors qui permet le cross-origin */
-/* pour authoriser l'appel du fichier entre backend et frontend*/
-require "cors.php";
-/*on appelle la fonction cors*/
+require("connectdblib.php");
+require("cors.php");
 cors();
 
-/* connexion à la db */
-require "./connectdblib.php";
-$bdd  = connectdb("hitw" , false );
-/* on a ajouté le type du fichier */
-header('Content-Type: application/json');
 
 
-/*
-    on construit un tableau php
-    ICI, on doir respecter le format demandé car les frontends  (dans cet exercice)
-    s'attendent à recevoir se format
-    
-    pour une première itération/intéraction avec les frontends
+$bdd=connectdb('hitw',true);
 
-*/
-$reponse = [
-    "error"         => true, /* indique si il y a une erreur ou non */
-    "error_message" => "Uknown error", /* il indique le message d'erreur pour les front */
-    "status"          => "" /* il sert à afficher se qu'on envoie aux front - les données de réponses */
-];
-if(empty($_REQUEST["nomDeParametre1"]) || empty($_REQUEST["nomDeParametre2"]) || empty($_REQUEST["nomDeParametre3"]) 
-|| empty($_REQUEST["nomDeParametre4"]) || empty($_REQUEST["nomDeParametre5"]))
+$sql="SELECT * FROM childs";
+$request= $bdd -> query($sql);
+
+$row=$request->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+if(isset($_REQUEST["lng"],$_REQUEST["lat"],$_REQUEST["missionId"],$_REQUEST["volunteerId"]))
 {
-    $response["error_message"] = "empty paramètre";
-    echo json_encode($response);
-    die();
+   //recuperer les params et les enregistrer dans des variables
+    $lng = $_REQUEST["lng"] ; 
+    $lat = $_REQUEST["lat"] ; 
+  
+    $missionId = $_REQUEST["missionId"] ; 
+    $volunteerId = $_REQUEST["volunteerId"] ; 
+   // $status = $_REQUEST["status"]; 
+ 
+    //requete sql
+    $sql = "INSERT INTO `posters`(`lng`, `lat`,missionId ,volunteerId) VALUES 
+    ($lng,$lat ,$missionId ,$volunteerId)";
+    $rep = $bdd->query($sql);
+    if(!$rep){
+        echo "error" ;
+    }else {
+        
+        echo "ok";
+    }
+   
 }
-if(!isset($_REQUEST["nomDeParametre1"],$_REQUEST["nomDeParametre2"],$_REQUEST["nomDeParametre3"],
-$_REQUEST["nomDeParametre4"],$_REQUEST["nomDeParametre5"]))
-{
-    $response["error_message"] = "Erreur paramètre";
-    echo json_encode($response);
-    die();
-}
-//recuperer les params et les enregistrer dans des variables
-$nomDeParametre1 = $_REQUEST["nomDeParametre1"] ; 
-$nomDeParametre2 = $_REQUEST["nomDeParametre2"] ; 
-$nomDeParametre3 = $_REQUEST["nomDeParametre3"] ; 
-$nomDeParametre4 = $_REQUEST["nomDeParametre4"] ; 
-$nomDeParametre5 = $_REQUEST["nomDeParametre5"] ; 
-
-//requete sql
-$sql = "INSERT INTO 
-    volunteers(lng , lat ,missionId,volunteerId,status)
-    VALUES
-    (:lng,:lat,:missionId,:volunteerId ,:status) ;";
-$stmtnt = $bdd->prepare($sql);
-
-$stmtnt->bindValue(":lng",$nomDeParametre1,PDO::PARAM_STR);
-$stmtnt->bindValue(":lat",$nomDeParametre2,PDO::PARAM_STR);
-$stmtnt->bindValue(":missionId",$nomDeParametre3,PDO::PARAM_STR);
-$stmtnt->bindValue(":volunteerId",$nomDeParametre4,PDO::PARAM_INT);
-$stmtnt->bindValue(":status",$nomDeParametre5,PDO::PARAM_STR;
-
-$stmtnt->execute();
-
-
-// on vérifie si il y a des données de la requête SQL (1 seul résultat) 
-if($stmtnt && $stmtnt->rowCount() > 0)
-{
-    //on récuèpre le résultat et on le met sur la detail
-    //on traite l'entrée du résultat de la requête
-    
-    //on met le nom et le prenom dans $reponse["details"]
-    $reponse["status"] = $detail;
-    //on dit qu'il n'y a pas d'erreur
-    $reponse["error"] = false;
-    //on dit qu'il n'y a pas d'erreur donc pas de message d'erreur
-    $reponse["error_message"] = "";
-}
-else
-{
-    //on affiche le message si la condition n'est pas remplie (pas d'entrées dans ce cas)
-    $reponse["error_message"] = "Pas de données";
-}
-
-/* on convertit en json le tableau $reponse et on l'affiche avec echo*/
-echo json_encode($reponse);
-
-/*on termine le script*/
-die();
