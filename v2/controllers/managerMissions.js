d900e6fc7679fc9bbb739f6d917c4c8d548ae600
bastienhousiaux <bastienhousiaux@yahoo.fr>
2019-08-03 11:17:52 +0200

    managerMissions

--- v2/controllers/managerMissions.js
+++ v2/controllers/managerMissions.js
@@ -1,3 +1,8 @@
 function managerMissionsController(){
-    
+
+    for(var i=0;i<missions.length;i++){
+        if(missions[i].status==="pending"){
+            $("#missionsPanel").append("<h2>mission #"+missions[i].id+"<h2>");
+        }
+    }
 }
--- v2/js/viewManager.js
+++ v2/js/viewManager.js
@@ -2,7 +2,6 @@ var views=['roleSelection','managerMap','managerMissions','volunteersMap','volun
 
 function loadView(name){
     var indexView=views.indexOf(name);
-    console.log(name,indexView)
     if(indexView>=0){
         $('#main').load('./views/'+name+'.html',function(){
             $.getScript('./controllers/'+name+'.js',function(){
